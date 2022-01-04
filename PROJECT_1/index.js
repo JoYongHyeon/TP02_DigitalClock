4f80f5e9ef51c8420db472498f5f6380c5da7764
JoYongHyeon <whdydgus5778@gmail.com>
2022-01-04 20:50:22 +0900

    괄호의 유무에 작동의 큰 차이가 발생..

--- PROJECT_1/index.js
+++ PROJECT_1/index.js
@@ -10,6 +10,9 @@ window.onload = ()=>
         btnMouseOn[i].addEventListener('mouseout',  mouseOut);
     }
 }
+let second   = 00;
+let ms       = 00;
+let interval;
 
 function mouseOn(e)
 {
@@ -30,15 +33,24 @@ function timeStart()
 {
     // setInterval(function(){...}, 지연시간);
     // 1000 당 1초로 계산
-    let seconds     = document.querySelector('#seconds');
-    let tens        = document.querySelector('#tens');
-    let second = 00;
-    let ms     = 00;
+    let seconds    = document.querySelector('#seconds');
+    let tens       = document.querySelector('#tens');
+    
+    // (괄호)의 여부. 괄호가 있고 없고는 매우 큰차이이며 괄호가 있으면 함수를 호출한다는 것이다.
+    // 괄호가 없다면 함수를 대입하고 있다고 생각하면 될 것 같다.
+    // 이렇게 되면 startTimer 는 함수의 반환값인 return문을 반환하는게 아니고 자신이 참조하고 있는 함수의 참조값을 저장하게 된다.
 
-    //  업데이트 상황
-    setInterval(()=> 
-    {
-        ms++;
+    // ★나만의 결론★
+    // startTimer() : function startTimer(){} 라는 함수를 호출,
+    // startTimer   : function startTimer의 주소 값을 저장하는것. 포인터 같은 느낌이라고 우선 알아두고 가자! 
+    // ※ 이 차이를 알지못해 setInterval 타이머의 0.01초마다 작동하지않고 처음에 딱 한번만 작동된것같다...
+    clearInterval(interval);
+    interval = setInterval(startTimer, 10);   
+}
+
+function startTimer()
+{
+    ms++;
         
         if(ms <= 9)
         {
@@ -61,57 +73,17 @@ function timeStart()
                 seconds.innerText = second;
             }
         }
-
-
-   
-        
-        // if(ms > 99)
-        // {
-        //     ms = 0;
-        //     second += 1;
-
-        //     if(second < 10)
-        //     {
-        //         seconds.innerText = "0" + second;
-        //     }
-        //     else{
-        //         seconds.innerText = second;
-        //     }         
-        // }
-
-    }, 10);
 }
-
 
 function timeStop()
 {
-    // 종료할땐 clearInterval 함수쓰면될듯
-    alert('stop');
+    clearInterval(interval);
 }
 function timereset()
 {
-    alert('Reset');
+    clearInterval(interval);
+    second = "00";
+    ms     = "00";
+    tens.innerText = ms;
+    seconds.innerText = second;
 }
-
-       
-        
-// setInterval(()=> 
-//     {
-//         tens.innerText = ms;
-//         ms += 1;
-        
-//         if(ms > 99)
-//         {
-//             ms = 0;
-//             second += 1;
-
-//             if(second < 10)
-//             {
-//                 seconds.innerText = "0" + second;
-//             }
-//             else{
-//                 seconds.innerText = second;
-//             }         
-//         }
-
-//     }, 10);
